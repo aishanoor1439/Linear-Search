1cba0492eefcd55f93570bff23963cf859426250
aishanoor1439 <aisha.noor1439@gmail.com>
2025-03-22 09:15:30 +0100

    Final Commit
    completed

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int location = 0;
+            int x = 30;
+            int[] a = { 10, 20, 30, 40, 50 };
+            int n = a.Length;
+            int i = 1;
+            while(i <= n && x != a[i])
+            {
+                i++;
+                if(x == a[i])
+                {
+                    location = i;
+                }
+                Console.WriteLine(location);
+            }
+
+
+        }
+    }
+}
